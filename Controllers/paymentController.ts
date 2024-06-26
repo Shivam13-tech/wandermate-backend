1b420805936d9bf25cdd01797fd06d0d80dcc7c6
Shivam Pandey <shivampandey88@hotmail.com>
2024-06-26 23:38:22 +0530

    Updated payment and tour content

--- Controllers/paymentController.ts
+++ Controllers/paymentController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import dotenv from "dotenv";
+import User from "../Models/authModel";
 
 dotenv.config({ path: ".env" });
 
@@ -12,13 +13,51 @@ interface Item {
 }
 
 const storeItems: any = new Map([
-  //productid, price and name of product
-  [1, { priceInCents: 10000, name: "learn react" }],
-  [2, { priceInCents: 20000, name: "learn css" }],
+  [
+    "667c408a3e322832859f07cc",
+    { priceInCents: 150000, name: "Mystical Himalayan Trek" },
+  ],
+  [
+    "667c40ef3e322832859f07d3",
+    { priceInCents: 80000, name: "Desert Safari Experience" },
+  ],
+  [
+    "667c414b3e322832859f07da",
+    { priceInCents: 120000, name: "Coastal Adventure Retreat" },
+  ],
+  [
+    "667c41903e322832859f07e1",
+    { priceInCents: 100000, name: "Cultural Heritage Trail" },
+  ],
+  [
+    "667c41ec3e322832859f07e8",
+    { priceInCents: 180000, name: "Alpine Lakes Expedition" },
+  ],
+  [
+    "667c422c3e322832859f07ef",
+    { priceInCents: 140000, name: "Wildlife Safari Adventure" },
+  ],
+  [
+    "667c42763e322832859f07f6",
+    { priceInCents: 250000, name: "Northern Lights Expedition" },
+  ],
+  [
+    "667c42d73e322832859f07fb",
+    { priceInCents: 160000, name: "Tropical Island Getaway" },
+  ],
+  [
+    "667c432a3e322832859f0800",
+    { priceInCents: 190000, name: "Volcano Exploration Journey" },
+  ],
+  [
+    "667c3fd53e322832859f07c6",
+    { priceInCents: 90000, name: "Sacred Temples Pilgrimage" },
+  ],
 ]);
 
 export const createCheckout = async (req: Request, res: Response) => {
   try {
+    const { userId, tourId } = req.body;
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
@@ -35,15 +74,41 @@ export const createCheckout = async (req: Request, res: Response) => {
           quantity: item.quantity,
         };
       }),
-      success_url: `${process.env.CLIENT_URL}/tour`,
+      success_url: `${process.env.CLIENT_URL}/booking`,
       cancel_url: `${process.env.CLIENT_URL}`,
     });
-    res.json({ url: session.url });
+    // Update user document to push the newly bought tourId
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $push: { boughtTours: tourId } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found or unable to update bought tours.",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: `Tour ID ${tourId} successfully added to user ${userId}'s bought tours.`,
+      user: updatedUser,
+      url: session.url,
+    });
   } catch (error) {
-    console.log(error);
+    console.error("Error updating user's bought tours:", error);
     return res.status(500).json({
-      status: "Something went wrong! Try again later",
-      message: error,
+      status: "error",
+      message: "Failed to update user's bought tours. Please try again later.",
     });
   }
+  // res.json({ url: session.url });
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(500).json({
+  //     status: "Something went wrong! Try again later",
+  //     message: error,
+  //   });
+  // }
 };
--- Models/authModel.ts
+++ Models/authModel.ts
@@ -1,73 +1,85 @@
-import mongoose, {Document, Schema} from 'mongoose';
-import validator from 'validator';
-import bcrypt from 'bcrypt';
+import mongoose, { Document, Schema } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
-interface Iuser extends Document{
-    name: string,
-    email: string,
-    gender: string,
-    password: string,
-    confirmPassword: string | undefined,
-    correctPassword(candidatePassword: string, userPassword: string): Promise<boolean>; //Returns a promise that will eventually return a boolean value
+interface Iuser extends Document {
+  name: string;
+  email: string;
+  gender: string;
+  password: string;
+  confirmPassword: string | undefined;
+  correctPassword(
+    candidatePassword: string,
+    userPassword: string
+  ): Promise<boolean>; //Returns a promise that will eventually return a boolean value
+  boughtTours: mongoose.Types.ObjectId;
 }
 
-
 const userSchema: Schema<Iuser> = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide your name'],
+  name: {
+    type: String,
+    required: [true, "Please provide your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a valid email"],
+    unique: true,
+    validate: {
+      validator: async function (value: string) {
+        const count = await mongoose
+          .model("User", userSchema)
+          .countDocuments({ email: value });
+        if (count === 0) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      message:
+        "Email already in use. Please provide a different email id to sign up",
     },
-    email: {
-        type: String,
-        required: [true, 'Please provide a valid email'],
-        unique: true,
-        validate: {
-            validator: async function (value: string){
-                const count = await mongoose.model('User', userSchema)
-                .countDocuments({ email: value });
-                if(count === 0){
-                    return true
-                }else{
-                    return false
-                }
-            },
-        message: "Email already in use. Please provide a different email id to sign up",
-        } 
+  },
+  gender: {
+    type: String,
+    required: [true, "Please mention your gender"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (this: Iuser, confirmpass: string): boolean {
+        return confirmpass === this.password;
+      },
+      message: "Passwords are not the same!",
     },
-    gender: {
-        type: String,
-        required: [true, 'Please mention your gender'],
+  },
+  boughtTours: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tour", // Reference to your Tour model
     },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minlength: 8,
-    },
-    confirmPassword: {
-        type: String,
-        required: [true, 'Please confirm your password'],
-        validate: {
-          validator: function(this: Iuser, confirmpass: string): boolean {
-            return confirmpass === this.password;
-          },
-        message: 'Passwords are not the same!'
-    }
-    }
+  ],
 });
 
-userSchema.pre<Iuser>('save', async function(next) {  // This will make sure confirmpassword is not written to db
-    this.password = await bcrypt.hash(this.password, 12);
-    this.confirmPassword = undefined;
-    next();
+userSchema.pre<Iuser>("save", async function (next) {
+  // This will make sure confirmpassword is not written to db
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
-userSchema.methods.correctPassword = async function(
-    candidatePassword: string,
-    userPassword: string 
-  ) {
-    return await bcrypt.compare(candidatePassword, userPassword);
+userSchema.methods.correctPassword = async function (
+  candidatePassword: string,
+  userPassword: string
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
